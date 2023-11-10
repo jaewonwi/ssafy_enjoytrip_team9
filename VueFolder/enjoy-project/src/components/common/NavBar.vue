9861b303f4c8d7b5660167b7d585ebe964be59d0
jaewonwi <jww5555@naver.com>
2023-11-10 12:44:33 +0900

    add board commit

--- VueFolder/enjoy-project/src/components/common/NavBar.vue
+++ VueFolder/enjoy-project/src/components/common/NavBar.vue
@@ -15,20 +15,20 @@
         <a href="/main" class="logo m-0">Tour <span class="text-primary">.</span></a>
         <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-right">
           <li class="active">
-            <router-link to="/" class="text-primary">Home</router-link>
+            <router-link to="/main" class="text-primary">Home</router-link>
           </li>
-          <li><router-link to="/">게시판</router-link></li>
+          <li><router-link to="/board">게시판</router-link></li>
           <li><a href="#">Search</a></li>
           <li><a href="map/map.html">Map</a></li>
 
           <!-- User 기능  -->
-          <li><a href="login.html">Sign In</a></li>
+          <li><a href="/login">Sign In</a></li>
           <li class="has-children">
             <a href="#">My Page</a>
             <ul class="dropdown">
-              <li><a href="mypage.html">My Info</a></li>
+              <li><a href="#">My Info</a></li>
               <li><a href="#">Bookmark</a></li>
-              <li><a href="/goLogout">Logout</a></li>
+              <li><a href="#">Logout</a></li>
             </ul>
           </li>
         </ul>
--- VueFolder/enjoy-project/src/router/index.js
+++ VueFolder/enjoy-project/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '@/components/MainPage.vue'
 import RegistPage from '@/components/user/RegistPage.vue'
 import LoginPage from '@/components/user/LoginPage.vue'
+import BoardPage from '@/components/board/BoardPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/main',
+      component: MainPage
+    },
     {
       path: '/regist',
       component: RegistPage
@@ -12,6 +18,13 @@ const router = createRouter({
     {
       path: '/login',
       component: LoginPage
+    },
+    {
+      path: '/board',
+      component: BoardPage,
+      children: [
+        
+      ]
     },
   ]
 })

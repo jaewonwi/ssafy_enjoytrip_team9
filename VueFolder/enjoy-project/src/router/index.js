433722077ecfa6bba0f7f3494e1ff93e775b4f7a
hwalang <smdong97@gmail.com>
2023-11-10 10:21:14 +0900

    index.js: router로 경로 지정( /, /login, /board)

--- VueFolder/enjoy-project/src/router/index.js
+++ VueFolder/enjoy-project/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import RegistPage from '@/components/user/RegistPage.vue'
 import LoginPage from '@/components/user/LoginPage.vue'
+import MainPage from '@/components/MainPage.vue'
+import BoardPage from '@/components/board/BoardPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      component: MainPage
+    },
     {
       path: '/regist',
       component: RegistPage
@@ -12,6 +18,10 @@ const router = createRouter({
     {
       path: '/login',
       component: LoginPage
+    },
+    {
+      path: '/board',
+      component: BoardPage
     },
   ]
 })

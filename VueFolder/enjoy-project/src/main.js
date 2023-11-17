a587a8241f382324d6578d664138ba8e94dcd872
jaewonwi <jww5555@naver.com>
2023-11-17 11:20:14 +0900

    vue version 3.8, board_query edit, img upload ect

--- VueFolder/enjoy-project/src/main.js
+++ VueFolder/enjoy-project/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 app.use(router)
 

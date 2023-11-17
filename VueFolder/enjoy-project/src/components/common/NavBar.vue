4fb657fb95e87888b00fc76b4a93bcecc018d5e1
hwalang <smdong97@gmail.com>
2023-11-17 12:36:07 +0900

    네비게이션바 수정

--- VueFolder/enjoy-project/src/components/common/NavBar.vue
+++ VueFolder/enjoy-project/src/components/common/NavBar.vue
@@ -9,38 +9,43 @@
     <div class="site-mobile-menu-body"></div>
   </div>
 
-  <nav class="site-nav">
+  <nav class="navbar navbar-expand-lg navbar-light navbar-color">
     <div class="container">
-      <div class="site-navigation">
-        <a href="/" class="logo m-0 under-line-none">Tour</a>
-        <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-right">
-          <li class="active">
-            <router-link to="/" class="nav-link">Home</router-link>
+      <a href="/" class="logo m-0 under-line-none">Tour</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="burger ml-auto float-right d-inline-block d-lg-none light"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0 float-right">
+          <li class="active nav-item">
+            <router-link to="/" class="nav-link text-white">Home</router-link>
           </li>
-          <li><router-link to="/board" class="nav-link">게시판</router-link></li>
-          <li><a href="#">Search</a></li>
-          <li><a href="map/map.html">Map</a></li>
+          <li class="nav-item">
+            <router-link to="/board" class="nav-link text-white">게시판</router-link>
+          </li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Search</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Map</a></li>
 
           <!-- User 기능  -->
           <li class="nav-item" v-show="!loginStore.isLogin">
-            <router-link to="/login" class="nav-link">로그인</router-link>
+            <router-link to="/login" class="nav-link text-white">로그인</router-link>
           </li>
           <li class="nav-item" v-show="loginStore.isLogin">
-            <a href="#">마이페이지</a>
+            <a class="nav-link text-white" href="#">마이페이지</a>
           </li>
           <li class="nav-item" v-show="loginStore.isLogin">
-            <a href="#" @click="logout">로그아웃</a>
+            <a class="nav-link text-white" href="#" @click="logout">로그아웃</a>
           </li>
         </ul>
-
-        <a
-          href="#"
-          class="burger ml-auto mr-5 float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light"
-          data-toggle="collapse"
-          data-target="#main-navbar"
-        >
-          <span></span>
-        </a>
       </div>
     </div>
   </nav>
@@ -87,5 +92,9 @@ const logout = async () => {
 <style scoped>
 .under-line-none {
   text-decoration: none;
+}
+
+.navbar-color {
+  background-color: rgb(26, 55, 77);
 }
 </style>
